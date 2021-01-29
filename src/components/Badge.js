bb4523b0f772c8c32c058c36d710b8e848f7a0eb
roxanaguerra <sguerraapaza@gmail.com>
2021-01-29 09:50:55 -0500

    create badge - componente

---
+++ src/components/Badge.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Badge extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>Badge</h1>
+            <div>
+                <img src="" alt="Logo de la conferencia"/>
+            </div>
+            <div>
+                <img src="" alt="Avatar"/>
+                <h1>
+                    Roxana <br/> Guerra
+                </h1>
+            </div>
+            <div>
+                <p>Front end Developer</p>
+            </div>
+        </div>
+        );
+    }
+};
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -8,9 +8,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import Badge from './components/Badge';
+
+// const element = <h1>Hello, Platzi Badges!</h1>;
 
 const container = document.getElementById('app');
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(<Badge />, container);
